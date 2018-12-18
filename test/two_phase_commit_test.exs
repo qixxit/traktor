409328fd6dd60379cedeeb7170b6852edadd46fd
Tobias Kräntzer <tobias@qixxit.de>
2018-12-18 10:11:10 +0100

    Add test for the main functions to prepare and commit

--- test/two_phase_commit_test.exs
+++ test/two_phase_commit_test.exs
@@ -1,3 +1,20 @@
 defmodule TwoPhaseCommitTest do
   use ExUnit.Case
+
+  alias TwoPhaseCommit.Store.InMemory, as: Store
+  alias TwoPhaseCommit.Action.NoOp, as: Action
+
+  test "prepare and commit" do
+    assert {:ok, ref} = Store.start_link(%{})
+    assert {:ok, revision, state} = Store.get(ref)
+
+    assert {:ok, transaction, transaction_ref} =
+             TwoPhaseCommit.prepare(Action, state, [:foo, :bar], Store, ref, revision)
+
+    assert {:ok, new_state, new_revision, result} =
+             TwoPhaseCommit.commit(Action, state, transaction, Store, ref, transaction_ref)
+
+    assert result == [:foo, :bar]
+    assert {:ok, new_revision, new_state} = Store.get(ref)
+  end
 end
